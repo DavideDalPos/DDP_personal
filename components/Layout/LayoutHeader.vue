17c766d85838bbd041d8b557388f4e583827348a
Davide Dal Pos <daveliga@gmail.com>
2025-03-26 11:59:32 -0400

    Updates

--- components/Layout/LayoutHeader.vue
+++ components/Layout/LayoutHeader.vue
@@ -1,9 +1,8 @@
 <template>
   <header>
-    <div
-      class="fixed w-full z-50 bg-opacity-50 backdrop-blur-sm"
-      style="background-color: #ead7bb"
-    >
+    <div class="fixed w-full z-50 bg-[#e6d7b9] bg-opacity-50 backdrop-blur-sm shadow-lg">
+
+      
       <div
         class="container mx-auto h-16 flex justify-between items-center px-8 z-10"
       >
@@ -16,20 +15,32 @@
 
         <div class="lg:flex flex-row gap-6 text-sm hidden">
           <nav class="flex flex-row gap-8 items-center">
-            <NuxtLink
-              v-for="{ label, link } in menu"
-              :to="link"
-            >
-              {{ label }}
-            </NuxtLink>
-            <span>|</span>
-            <div class="flex flex-row gap-4 items-center">
-              <a href="https://x.com/CSE_at_FSCA"><IconX class="h-4" /></a>
-              <a href="https://www.facebook.com/CSE.InsectaMundi"
-                ><IconFacebook class="h-4"
-              /></a>
-            </div>
-          </nav>
+            <template v-for="{ label, link, submenu } in menu">
+             <div v-if="submenu" class="relative group flex items-center gap-1">
+               <button class="hover:underline flex items-center gap-1">
+                {{ label }}
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 transition-transform group-hover:rotate-180" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+               </button>
+              <div class="absolute left-0 top-full mt-1 shadow-md opacity-0 group-hover:opacity-100 transition-opacity" style="background-color: #ead7bb">
+                <ul class="flex flex-col w-40 py-0">
+                   <li v-for="{ label, link } in submenu" :key="link">
+                    <NuxtLink 
+          :to="link" 
+          class="block px-4 py-2 hover:bg-opacity-75 hover:bg-gray-200"
+        >
+          {{ label }}
+        </NuxtLink>
+                   </li>
+                </ul>
+              </div>
+             </div>
+            <NuxtLink v-else :to="link">{{ label }}</NuxtLink>
+            </template>
+         </nav>
+
+
         </div>
         <MobileNavbar :menu="menu" />
       </div>
@@ -50,15 +61,26 @@ const menu = [
   },
   {
     label: 'Insecta Mundi',
-    link: '/insecta_mundi'
+    submenu: [  // Submenu items
+      { label: 'Guidelines', link: '/guidelines' },
+      { label: 'Issues', link: '/insecta_mundi' }
+    ]
   },
   {
-    label: 'Publications',
-    link: '/publications'
+    label: 'Other Publications',
+    submenu: [  // Submenu items
+      { label: 'CSE Memoirs', link: '/publications' },
+      { label: 'CSE Newsletter', link: '/publications' },
+      { label: 'FSCA Publications', link: '/grants' }
+    ]
   },
   {
     label: 'Travel grants',
-    link: '/grants'
+    submenu: [  // Submenu items
+      { label: 'Peck Grant', link: '/grants' },
+      { label: 'How to Apply', link: '/howtoapply' },
+      { label: 'Previous report', link: '/proposal' },
+    ]
   },
   {
     label: 'Annual meetings',
